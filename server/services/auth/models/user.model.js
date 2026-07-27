5c44c9967cd125e248d4b0c78141693d9345c0aa
Arun-Jawlia <arunkumar08.mk@gmail.com>
2026-07-27 13:30:50 +0530

    feat: firebase setup

---
+++ server/services/auth/models/user.model.js
@@ -0,0 +1,25 @@
+import { Model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    firebaseUID: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = new Model("User", UserSchema);
+export { User };
