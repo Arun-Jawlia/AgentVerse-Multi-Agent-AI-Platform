1c5c5210f61d7d3a89ae369a61c222f98a0de271
Arun-Jawlia <arunkumar08.mk@gmail.com>
2026-08-31 17:43:19 +0530

    feat: ppt agent completed

--- server/services/agent/agents/ppt.agent.js
+++ server/services/agent/agents/ppt.agent.js
@@ -1,4 +1,7 @@
 import { getModel } from "../config/llmModel.js";
+import { generatePpt } from "../utils/generatePpt.js";
+import { getFromS3 } from "../utils/getFromS3.js";
+import { uploadToS3 } from "../utils/uploadToS3.js";
 
 export const pptAgent = async (state) => {
   try {
@@ -39,11 +42,39 @@ export const pptAgent = async (state) => {
             }
         `;
 
-    const res = await llm.invoke(prompt)
-    const data = JSON.parse(res.content)
-    
+    const res = await llm.invoke(prompt);
+    const data = JSON.parse(res.content);
+    const ppt = await generatePpt(data);
+    const buffer = await ppt.write({
+      outputType: "nodebuffer",
+    });
+
+    const filename = `ppt-${Date.now()}.pptx`;
+    await uploadToS3(
+      filename,
+      buffer,
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    );
+
+    const downloadUrl = await getFromS3(filename, 24 * 60 * 60);
+    return {
+      ...state,
+      aiResponse: `
+        # Presentation Generated
+
+        **${data.title}**
+
+        [DownloadPPT](${downloadUrl})
+
+        _Link expires in 10 minutes_
+        
+        `,
+    };
   } catch (error) {
-
-
+    console.log(error);
+    return {
+      ...state,
+      aiResponse: `Failed To Generate PPT`,
+    };
   }
 };
